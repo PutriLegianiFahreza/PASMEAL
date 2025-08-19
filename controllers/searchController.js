7a48a6b5b042d436eb06a945e02fca72d033ff5e
Putri-Legiani-Fahreza-BE9330552 <putri.213040039@mail.unpas.ac.id>
2025-08-19 20:49:31 +0700

    search all

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -7,13 +7,19 @@ const searchAll = async (req, res) => {
   }
 
   try {
+    // Cari kios berdasarkan nama
     const kiosResult = await pool.query(
       'SELECT * FROM kios WHERE nama_kios ILIKE $1 ORDER BY id DESC',
       [`%${query}%`]
     );
 
+    // Cari menu berdasarkan nama + include kios_id biar bisa di-group
     const menuResult = await pool.query(
-      'SELECT * FROM menu WHERE nama_menu ILIKE $1 ORDER BY id DESC',
+      `SELECT m.*, k.id AS kios_id, k.nama_kios 
+       FROM menu m
+       JOIN kios k ON m.kios_id = k.id
+       WHERE m.nama_menu ILIKE $1
+       ORDER BY m.id DESC`,
       [`%${query}%`]
     );
 
@@ -27,6 +33,5 @@ const searchAll = async (req, res) => {
     res.status(500).json({ message: 'Terjadi kesalahan server' });
   }
 };
-
 
 module.exports = { searchAll };
